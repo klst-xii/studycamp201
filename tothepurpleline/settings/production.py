32c7a43f55fd17e6adf6d3b04f9cedf8c2170e05
klstxii <ksarmiento@student.hau.edu.ph>
2022-11-12 22:41:51 +0800

    initial commit

--- tothepurpleline/settings/production.py
+++ tothepurpleline/settings/production.py
@@ -11,3 +11,15 @@ CSRF_COOKIE_SECURE              = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
 SECURE_HSTS_SECONDS             = 1000000
 SECURE_FRAME_DENY               = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
